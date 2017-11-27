36adecd92d09ef0d4c35b2df6606981f6c4bd763
ian klatzco <iklatzco@gmail.com>
2017-11-26 18:19:50 -0600

    cleanup

--- toplevel.sv
+++ toplevel.sv
@@ -6,29 +6,26 @@ module toplevel (
 	output logic [9:0] LEDG
 );
 
-logic Clk;
+logic Clk, reset;
 assign Clk = CLOCK_50;
-
-logic reset;
 always_ff @ (posedge Clk) begin
     reset <= ~(KEY[0]);
 end
 
-logic [7:0] keyCode;
-logic press;
-assign LEDG[0] = press;
-assign LEDG[9:1] = keyCode;
+logic [7:0] key_code;
+logic key_press;
+assign LEDG[9] = key_press;
+assign LEDG[8:0] = key_ccode;
 
-HexDriver hex_driver_0 ( .In0 (keyCode[3:0]), .Out0(HEX0) );
-HexDriver hex_driver_1 ( .In0 (keyCode[7:4]), .Out0(HEX1) );
-HexDriver hex_driver_2 ( .In0 (4'b1111), .Out0(HEX2) );
+HexDriver hex_driver_0 ( .In0 (key_code[3:0]), .Out0(HEX0) );
+HexDriver hex_driver_1 ( .In0 (key_code[7:4]), .Out0(HEX1) );
 
 keyboard keyboard_inst(
 	.Clk    (Clk),
 	.psClk  (PS2_KBCLK), .psData(PS2_KBDAT),
 	.reset  (reset),
-	.keyCode(keyCode),
-	.press  (press)
+	.keyCode(key_code),
+	.press  (key_press)
 );
 
 endmodule // toplevel
