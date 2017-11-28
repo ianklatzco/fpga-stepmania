85b367c0aca6f4f9e6c2af9b82a7adb43cee6c98
DashKosaka <32228463+DashKosaka@users.noreply.github.com>
2017-11-28 02:24:49 -0600

    made some basic arrow sprites...may need resizing. Also added the audio driver to helper tools

---
+++ ECE385-HelperTools/PNG To Hex/On-Chip Memory/sprite_bytes/right.sv
@@ -0,0 +1,39 @@
+/*
+    This sprite table was generated using 'conv_to_sv.py'. Find out more here: https://github.com/Atrifex/ECE385-HelperTools
+    To use, instantiate this module in your color mapper. The SpriteX input should be connected to
+        'ObjectXSize - DistX', where ObjectXSize is the width of your object in pixels along the
+        x direction. DistX is the horizontal distance between the DrawX pxiel and the top left corner
+        of the object in question, so something like: 'DistX = DrawX - ObjectXPosition' is fine.
+        Similarly this goes for SpriteY. Warning: If you don't do this, your image will be flipped along
+        the axis you ignored. This is a handy way to flip an image if you need to, though.
+ 
+    In the color mapper, you can then simply do something like:
+    module ColorMapper(...)
+    ...
+    logic [7:0] ObjectR, ObjectG, ObjectB
+    parameter ObjectXSize = 10'd10;
+    parameter ObjectYSize = 10'd10;
+    ...
+    always_comb
+    ...
+         if(ObjectOn == 1'b1)
+         begin
+             Red = ObjectR
+             Green = ObjectG
+             Blue = ObjectB
+         end
+     ...
+     ObjectSpriteTable ost(
+                           .SpriteX(ObjectXSize - DistX), .SpriteY(ObjectYSize - DistY),
+                           .SpriteR(ObjectR), .SpriteG(ObjectG), .SpriteB(ObjectB)
+                           );
+ 
+     See the comment at the top of the generation script if you're still confused.
+*/
+module right(input [9:0] SpriteX, SpriteY,
+            output [7:0] SpriteR, SpriteG, SpriteB);
+
+logic [9:0] X_Index, Y_Index;
+
+assign X_Index = SpriteX % 10'd8;
+assign Y_Index = SpriteY % 10'd8;
